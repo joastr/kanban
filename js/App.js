584d44559677c02d85abc8ad3c76923f1c4c8891
Joanna Kasperek <joastr1@gmail.com>
2018-12-09 12:34:45 +0100

    add API

---
+++ js/App.js
@@ -0,0 +1,54 @@
+// OGÓLNA FUNKCJA
+/*function randomString() {
+	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+	var str = '', i;
+	for (i = 0; i < 10; i++) {
+	  str += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return str;
+}
+*/
+function generateTemplate(name, data, basicElement) {
+  	var template = document.getElementById(name).innerHTML;
+  	var element = document.createElement(basicElement || 'div');
+  
+  	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+  
+  	return element;
+}
+
+//API KODILLA
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var myHeaders = {
+  'X-Client-Id': 'X-Client-Id',
+  'X-Auth-Token': 'X-Auth-Token'
+};
+
+fetch(prefix + baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
+
+  function setupColumns(columns) {
+    columns.forEach(function (column) {
+          var col = new Column(column.id, column.name);
+      board.addColumn(col);
+      setupCards(col, column.cards);
+      console.log('kolumna', columns);
+      console.log('kolumna col', col);
+      
+    });
+  }
+
+  function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
---
+++ js/Board.js
@@ -0,0 +1,55 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var myHeaders = {
+  'X-Client-Id': 'X-Client-Id',
+  'X-Auth-Token': 'X-Auth-Token'
+};
+
+
+var board = {
+    name: 'Tablica Kanban',
+    addColumn: function(column) {
+      this.element.appendChild(column.element);
+      //initSortable(column.id); //About this feature we will tell later
+    },
+    element: document.querySelector('#board .column-container')
+};
+
+
+document.querySelector('#board .create-column').addEventListener('click', function() {
+    var name = prompt('Enter a column name');
+    var data = new FormData();
+  
+    data.append('name', name);
+  
+    fetch(prefix + baseUrl + '/column', {
+        method: 'POST',
+        headers: myHeaders,
+        body: data,
+      })
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(function(resp) {
+        var column = new Column(resp.id, name);
+        board.addColumn(column);
+      });
+});
+
+
+/*
+document.querySelector('#board .create-column').addEventListener('click', function() {
+    var name = prompt('Enter a column name');
+    var column = new Column(name);
+    board.addColumn(column);
+});
+	
+function initSortable(id) {
+  	var el = document.getElementById(id);
+  	var sortable = Sortable.create(el, {
+    	group: 'kanban',
+    	sort: true
+  	});
+}
+
+*/
